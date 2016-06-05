319a8ae24e6306e39f95a0e7539cda655803d42c
jwhitis <jack.whitis@gmail.com>
2016-06-05 09:11:01 -0500

    Style home page and photo preview page

--- app/assets/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-$(document).on("ready page:load", function() {
-
-  window.setTimeout(function() {
-    $("div.flash-message").alert("close");
-  }, 15000);
-
-});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,21 @@
 module ApplicationHelper
 
-  def flash_message_classes name
-    classes = %w(alert fade in flash-message alert-dismissible)
-    classes << "alert-info" if name == "notice"
-    classes << "alert-danger" if name == "alert"
-    classes.join(" ")
+  def header_has_callout?
+    content_for(:header) =~ /callout/
+  end
+
+  def pre_order_button options = {}
+    options[:class] = "button " + options[:class].to_s
+
+    link_to "javascript:void(0)", options do
+      fa_icon("angle-double-right") + "Pre-Order the Album"
+    end
+  end
+
+  def star number
+    content_tag(:div, class: "star") do
+      fa_icon("star") + content_tag(:span, number)
+    end
   end
 
 end
